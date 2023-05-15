1c128c118f180e743b19ecb982cb037f13feb68b
andi lovetto <andilovetto@gmail.com>
2023-05-14 20:50:29 -0600

    Write prepend and insert methods

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -4,15 +4,22 @@ class LinkedList
         @head = nil
     end
 
+    def find_tail
+        current_node = @head
+        return current_node unless current_node.next_node
+        return current_node unless current_node.next_node while current_node = current_node.next_node
+    end
+
     def append(beat)
-        if @head == nil
-            @head = Node.new(beat)
+        if @head
+            find_tail.next_node = Node.new(beat)
         else
-            #require 'pry'; binding.pry
-            @head.next_node = Node.new(beat)
+            
+            @head = Node.new(beat)
         end
         beat
     end
+
     def count
         return 0 if @head == nil
         counter = 1
@@ -23,16 +30,36 @@ class LinkedList
         end
         counter
     end
+
     def to_string
         return "" if @head == nil
         return @head.data if @head.next_node == nil
+       
         the_beat = @head.data #doop 
         current_node = @head.next_node # node instance with value/data attr of deep
-        #require 'pry'; binding.pry
+    
         while current_node.next_node != nil do
-
-            #require 'pry'; binding.pry
+            the_beat << " " +  current_node.data
+            current_node = current_node.next_node
         end
         the_beat << " " +  current_node.data
+    end
+    
+    def prepend(beat)
+        old_head = @head
+        @head = Node.new(beat)
+        @head.next_node = old_head
+        beat
+    end
+
+    def insert(position, beat)
+        tail = @head
+        previous = nil
+        position.times do
+            previous = tail
+            tail = tail.next_node
+        end
+        previous.next_node = Node.new(beat)
+        previous.next_node.next_node = tail
     end
 end 
